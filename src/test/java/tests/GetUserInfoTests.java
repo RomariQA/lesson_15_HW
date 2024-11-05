1480e07081fe66039f56fbd8dbb81642a09c6692
Maya Romanovich <mariaromanovich@yandex.ru>
2024-11-06 02:18:42 +0300

    added fixes

--- src/test/java/tests/GetUserInfoTests.java
+++ src/test/java/tests/GetUserInfoTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.logevents.SelenideLogger.step;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.GetUserInfoSpec.*;
